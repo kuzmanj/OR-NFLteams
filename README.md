7150e9b8e7abbf9afe38e9d993169828e04bb717
Jan Kuzman <kuzmanjan@gmail.com>
2024-11-09 16:01:28 +0100

    Gotovo

---
+++ README.md
@@ -0,0 +1,34 @@
+# Otvoreno racunarstvo
+
+
+## Skup podataka NFL timovi
+Ovaj skup podataka sadrži informacije o 15 NFL timova i njihovih random izabranih 5 igrača. Svaki tim ima detalje poput naziva, grada, godine osnivanja, konferencije, divizije, trenera, osvojenih naslova, web-stranice i vlasika, dok su igrači povezani s timovima i uključuju podatke poput imena, prezimena, pozicije i broja dresa.
+
+## Metapodaci
+
+- **Opis otvorene licencije:** Creative Commons Attribution 4.0 International (CC BY 4.0)
+- **Naziv autora:** Jan Kuzman
+- **Verzija skupa podataka:** 1.0
+- **Jezik:** Engleski
+- **Izvor:** Službene NFL stranice
+- **Datum:** 25.10.2024.
+- **Potencijalno ažuriranje:** Jednom godišnje
+- **Kontakt:** kuzmanjann@gmail.com
+- **Format podataka:** JSON, CSV
+- **Opis atributa:**
+  - `id`: Jedinstveni identifikator tima/igrača
+  - `name`: Naziv NFL tima
+  - `city`: Grad u kojem tim igra
+  - `founded`: Godina kada je tim osnovan
+  - `stadium`: Ime stadiona na kojem tim igra
+  - `coach`: Ime glavnog trenera tima
+  - `conference`: Konferencija kojoj tim pripada
+  - `titles`: Broj osvojenih naslova
+  - `name`: Ime igrača
+  - `surname`: Prezime igrača
+  - `position`: Pozicija igrača na terenu
+  - `jersey_number`: Broj dresa igrača
+  - `owner`: Ime vlasnika tima
+  - `division`: Divizija u kojoj se tim natječe
+  - `web-page`: Link na web stranicu NFL tima
+
---
+++ backend/server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const { Client } = require("pg");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+const PORT = 5004;
+
+const client = new Client({
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+  })
+);
+
+app.use("/public", express.static("public"));
+
+client.connect();
+
+app.get("/api/teams", async (req, res) => {
+  const search = req.query.search || "";
+  const attribute = req.query.attribute || "";
+
+  let query = "SELECT * from teams JOIN players ON teams.id = players.team";
+  const values = [];
+
+  if (search) {
+    if (attribute === "wildcard") {
+      query += ` WHERE name ILIKE $1 
+                 OR city ILIKE $1 
+                 OR stadium ILIKE $1 
+                 OR division ILIKE $1 
+                 OR conference ILIKE $1 
+                 OR coach ILIKE $1 
+                 OR owner ILIKE $1 
+                 OR webpage ILIKE $1 
+                 OR CAST(founded AS TEXT) ILIKE $1 
+                 OR CAST(titles AS TEXT) ILIKE $1
+                 OR playername ILIKE $1 
+                 OR playersurname ILIKE $1
+                 OR CAST(jerseynumber AS TEXT) ILIKE $1
+                 OR position ILIKE $1`;
+      values.push(`%${search}%`);
+    } else if (["titles", "founded", "jerseynumber"].includes(attribute)) {
+      query += ` WHERE ${attribute} = $1`;
+      values.push(parseInt(search));
+    } else {
+      query += ` WHERE ${attribute} ILIKE $1`;
+      values.push(`%${search}%`);
+    }
+  }
+
+  try {
+    let result = null;
+    if (search) {
+      result = await client.query(query, values);
+    } else {
+      result = await client.query(query);
+    }
+    res.json(result.rows);
+  } catch (err) {
+    console.log(err);
+  }
+});
+//ewerfwefwew
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
